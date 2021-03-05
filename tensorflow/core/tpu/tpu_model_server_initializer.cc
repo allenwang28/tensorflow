b85c6237bfc0e523ab457228aab4097f54717ad5
Allen Wang <allencwang@google.com>
2021-03-05 13:21:07 -0800

    remove dlsym

--- tensorflow/core/tpu/tpu_model_server_initializer.cc
+++ tensorflow/core/tpu/tpu_model_server_initializer.cc
@@ -14,7 +14,6 @@ limitations under the License.
 ==============================================================================*/
 
 #include "tensorflow/core/tpu/tpu_model_server_initializer.h"
-#include "tensorflow/core/tpu/tpu_api_dlsym_initializer.h"
 
 #include <dlfcn.h>
 
